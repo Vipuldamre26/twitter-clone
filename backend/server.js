6306c7dc5992def2cd0b3612ab42e97eb354b280
Vipul <vipuldamrepro26@gmail.com>
2024-11-11 21:34:52 +0530

    Impliment notification controller

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/auth.routes.js';
+import userRouter from './routes/user.routes.js';
+
 import connectMongoDB from './db/connectMongoDB.js';
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
@@ -24,5 +26,8 @@ connectMongoDB();
 // routes 
 
 app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
